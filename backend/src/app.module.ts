0741a0bbf1beb416673b2b408c9f4df7c2a17448
JovanAnanta <jovanananta51@gmail.com>
2026-08-28 19:28:45 +0700

    fix: force update app.module.ts import spelling for CI

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -14,7 +14,7 @@ import { RolePermissionModule } from './modules/system/role-permission/role-perm
 import { SystemConfigurationModule } from './modules/system/system-configuration/system-configuration.module.js';
 
 import { CustomerModule } from './modules/sales/customer/customer.module.js';
-import { SupplierModule } from './modules/purchasing/suplier.module.js';
+import { SupplierModule } from './modules/purchasing/supplier.module.js';
 import { CategoryModule } from './modules/master-data/category/category.module.js';
 import { UnitModule } from './modules/master-data/unit/unit.module.js';
 import { ProductModule } from './modules/master-data/product/product.module.js';
